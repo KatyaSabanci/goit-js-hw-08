b8bf0cda2934e41426ddcd53f49be6b59a9d4c24
KatyaSabanci <kateryna.kosyachenko@gmail.com>
2023-10-01 22:17:08 +0300

    video

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const photoElements = document.querySelector('.gallery');
+const itemsMarkup = createPhotoMarkup(galleryItems);
 
-console.log(galleryItems);
+function createPhotoMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
+photoElements.insertAdjacentHTML('beforeend', itemsMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+player.on(
+  'timeupdate',
+  throttle(function (onPlay) {
+    localStorage.setItem('videoplayer-current-time', onPlay.seconds);
+  }, 1000)
+);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+}
+player.setCurrentTime(savedTime);
